5be33c52204664947c2fdd383c6b828025debb30
Ralf Gommers <ralf.gommers@googlemail.com>
2014-04-30 00:17:07 +0200

    TST: fix test precision (atol=0 doesn't work well) and some style issues.

--- pywt/tests/test_multidim.py
+++ pywt/tests/test_multidim.py
@@ -3,8 +3,7 @@
 from __future__ import division, print_function, absolute_import
 
 import numpy as np
-from numpy.testing import (run_module_suite, assert_almost_equal,
-                           assert_allclose, assert_)
+from numpy.testing import run_module_suite, assert_allclose, assert_
 
 import pywt
 
@@ -23,7 +22,8 @@ def test_idwtn_reconstruct():
 
     # idwtn creates even-length shapes (2x dwtn size)
     original_shape = [slice(None, s) for s in data.shape]
-    assert_allclose(data, pywt.idwtn(d, wavelet)[original_shape])
+    assert_allclose(data, pywt.idwtn(d, wavelet)[original_shape],
+                    rtol=1e-13, atol=1e-13)
 
 
 def test_idwtn_idwt2():
@@ -38,7 +38,7 @@ def test_idwtn_idwt2():
     d = {'aa': LL, 'da': HL, 'ad': LH, 'dd': HH}
 
     assert_allclose(pywt.idwt2((LL, (HL, LH, HH)), wavelet),
-                    pywt.idwtn(d, wavelet))
+                    pywt.idwtn(d, wavelet), rtol=1e-14, atol=1e-14)
 
 
 def test_idwtn_missing():
@@ -47,14 +47,14 @@ def test_idwtn_missing():
         [0, 4, 1, 5, 1, 4],
         [0 ,5, 6, 3, 2, 1],
         [2, 5,19, 4,19, 1]])
-    
+
     wavelet = pywt.Wavelet('haar')
 
     LL, (HL, _, HH) = pywt.dwt2(data, wavelet)
     d = {'aa': LL, 'da': HL, 'dd': HH}
 
     assert_allclose(pywt.idwt2((LL, (HL, None, HH)), wavelet),
-                    pywt.idwtn(d, 'haar'))
+                    pywt.idwtn(d, 'haar'), atol=1e-15)
 
 
 def test_idwtn_take():
@@ -71,6 +71,7 @@ def test_idwtn_take():
 
     assert_(data.shape != pywt.idwtn(d, wavelet).shape)
     assert_allclose(data, pywt.idwtn(d, wavelet, data.shape), atol=1e-15)
+
 
 if __name__ == '__main__':
     run_module_suite()
